ab21ff9adf3b90994338c2058c5204046bbfbab7
Ragnar Homsar <redmage4@gmail.com>
2018-03-02 18:28:20 -0800

    initial non-VS commit

--- StatEdit/StatEditForm.cs
+++ StatEdit/StatEditForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StatEdit
 {
-    public partial class Form1 : Form
+    public partial class StatEditForm : Form
     {
-        public Form1()
+        public StatEditForm()
         {
             InitializeComponent();
         }
